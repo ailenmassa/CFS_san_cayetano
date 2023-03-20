2cc450ad498e5e9f0ef19f4501dca328e1a91203
ailenmassa <127357292+ailenmassa@users.noreply.github.com>
2023-03-20 20:48:16 -0300

    Update Lista.ts

--- Lista.ts
+++ Lista.ts
@@ -1,1 +1,6 @@
-console.log("Preparar un batido de banana en 5 pasos:")("1-Coloca 1 o 2 plátanos en rodajas en la licuadora")("2-Añade 1/2 taza de leche")("3-Añade una taza de hielo")("4-Licua los ingredientes hasta obtener una mezcla homogénea.")("5-Agrega azucar a gusto")
+console.log("Preparar un batido de banana en 5 pasos:")
+console.log("1-Coloca 1 o 2 plátanos en rodajas en la licuadora")
+console.log("2-Añade 1/2 taza de leche")
+console.log("3-Añade una taza de hielo")
+console.log("4-Licua los ingredientes hasta obtener una mezcla homogénea.")
+console.log("5-Agrega azucar a gusto")
