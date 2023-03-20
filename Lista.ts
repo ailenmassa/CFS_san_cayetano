dfbfbe5413b7a8963cceb39aec7018dff772281b
ailenmassa <127357292+ailenmassa@users.noreply.github.com>
2023-03-20 20:03:28 -0300

    lista de cinco pasos

---
+++ Lista.ts
@@ -0,0 +1,1 @@
+console.log("Preparar un batido de banana en 5 pasos:")("1-Coloca 1 o 2 plátanos en rodajas en la licuadora")("2-Añade 1/2 taza de leche")("3-Añade una taza de hielo")("4-Licua los ingredientes hasta obtener una mezcla homogénea.")("5-Agrega azucar a gusto")
